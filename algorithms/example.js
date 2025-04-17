78caffd21221b2deb3b12b6a4d8c615645d938e2
CodeByMaganga <codebymaganga@gmail.com>
2025-04-17 20:59:15 +0300

    new class notes

---
+++ algorithms/example.js
@@ -0,0 +1,20 @@
+
+let someString = ' gomycode'
+
+let string1
+
+function removeTrailingWhitespaces(string){
+
+       return string.trimStart()
+}
+
+
+console.log(removeTrailingWhitespaces(someString))
+
+console.log(removeTrailingWhitespaces(' Remiovewhotw'))
+ 
+console.log(removeTrailingWhitespaces(' regwrgw'))
+
+
+
+//parameters vs arguments
---
+++ javascriptFoundation/index.js
@@ -0,0 +1,87 @@
+
+//writes sth inside our terminal
+console.log('Hello World')
+
+//this comment is only on line 5
+
+/*
+sfbs
+flbs
+sfb
+sfbs
+sfb
+sbf
+this is a multi line comment
+*/
+
+/**
+ * 6 Data Types ---> primitive
+ * Boolean---> True or False
+ * Number -----> Integers
+ * Strings ---> Characters and words
+ * Null------>means nothing
+ * Undefined------> Variable has no value
+ * 
+ * symbol----->look later in the course
+ * 
+ */
+
+//typeof ----> tells type of data
+console.log(typeof 6)
+
+
+//ARITHMETIC OPERATORS
+
+/**
+ * addition->+
+ * subtraction-> -
+ * multiplication -> *
+ * division -> /
+ * modulo --> % ---> return the remainder of a division
+ * exponential ---> ** ---> raises the value to the base provided
+ * 
+ */
+
+
+console.log("string" * 2)
+
+//string concatenation
+let firstName = 'Some'
+let lastName = 'Body'
+firstName = lastName
+console.log(firstName + " " + lastName) 
+
+let number = 2
+
+let number2 = 4
+
+
+console.log( number+=number2)
+
+console.log( 5 != '5')
+
+/**
+ *  ==  ------> checks equality without checking for data type
+ *  === -----> checks equality and  checks for data type
+ * 
+ * 
+ * ! ====> for the opposite value
+ * 
+ * 
+ * 
+ */
+
+console.log(firstName && lastName)
+
+
+
+
+
+
+
+
+
+
+
+
+
